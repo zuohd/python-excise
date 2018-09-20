12597edbd9da7d641275ed5c9ca97c4251d0c488
zuohd <zuo.houde@gmail.com>
2018-09-21 04:54:12 +0800

    read commandline or config file parameter to run server

--- tornado/server04.py
+++ tornado/server04.py
@@ -5,7 +5,7 @@ import tornado.options
 
 # define parameter,like --port=9000 list=a,b,c,de,
 tornado.options.define("port", default=8000, type=None)
-tornado.options.define("list", default=[], type=str)
+tornado.options.define("list", default=[], type=str,multiple=True)
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -14,14 +14,14 @@ class IndexHandler(tornado.web.RequestHandler):
 
 
 if __name__ == '__main__':
-    # convert command arguments  and save into tornado.options object
-    tornado.options.parse_command_line()
+    tornado.options.parse_config_file("config")
     print(tornado.options.options.list)
     app = tornado.web.Application([
         (r"/", IndexHandler)
     ])
 
     httpserver = tornado.httpserver.HTTPServer(app)
+
     # use parameter value
     httpserver.bind(tornado.options.options.port)
     httpserver.start(1)
