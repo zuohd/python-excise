908189a71a70e0e97c159518d33fe5961e33166d
zuohd <zuo.houde@gmail.com>
2018-09-21 08:11:18 +0800

    route handlers

--- tornado_project/application.py
+++ tornado_project/application.py
@@ -7,6 +7,13 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', index.IndexHandler),
-            (r'/soderberg',index.SoderbergHandler,{"age":30,"number":"123456"})
+            (r'/soderberg',index.SoderbergHandler,{"age":30,"number":"123456"}),
+            (r'/json1',index.Json1Handler),
+            (r'/json2', index.Json2Handler),
+            (r'/header',index.HeaderHandler),
+            (r'/status',index.StatusHandler),
+            (r'/index',index.RedirectHandler),
+            #iseror?flag=2
+            (r'/iserror',index.ErrorHandler)
         ]
         super(Application, self).__init__(handlers,**config.settings)
--- tornado_project/views/index.py
+++ tornado_project/views/index.py
@@ -14,4 +14,72 @@ class SoderbergHandler(RequestHandler):
         self.number = number
 
     def get(self, *args, **kwargs):
-        self.write("yes,we can.%d-%s"%(self.age,self.number))
+        self.write("yes,we can.%d-%s" % (self.age, self.number))
+
+
+import json
+
+
+class Json1Handler(RequestHandler):
+    def get(self, *args, **kwargs):
+        per = {
+            "name": "Jack",
+            "age": 45,
+            "height": 175,
+            "weight": 80
+        }
+        jsonStr = json.dumps(per)
+        self.set_header("Content-Type", "application/json;charset=utf-8")
+        self.set_header("name", "xxxx")
+        self.write(jsonStr)
+
+
+class Json2Handler(RequestHandler):
+    def get(self, *args, **kwargs):
+        per = {
+            "name": "Jack",
+            "age": 45,
+            "height": 175,
+            "weight": 80
+        }
+        self.write(per)
+
+
+class HeaderHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Content-Type", "application/html;charset=utf-8")
+
+    def get(self, *args, **kwargs):
+        pass
+
+    def post(self, *args, **kwargs):
+        pass
+
+
+class StatusHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        self.set_status(404)
+        self.write("********************")
+
+
+class RedirectHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        self.redirect("/")
+
+
+class ErrorHandler(RequestHandler):
+    def write_error(self, status_code, **kwargs):
+        if status_code==500:
+            #500 page
+            self.write("server internal error")
+        elif status_code==404:
+            #404 page
+            self.write("Not found")
+        else:
+            self.write("don't know")
+        self.set_status(status_code)
+    def get(self, *args, **kwargs):
+        flag=self.get_query_argument("flag")
+        if flag=='0':
+            self.send_error(500)
+        self.write("right.")
