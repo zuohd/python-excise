e5ba7b594c3e46f6da3c03af9cec9c124a27aa88
zuohd <zuo.houde@gmail.com>
2018-09-24 03:56:32 +0800

    refactor code

--- tornado_project/views/student.py
+++ tornado_project/views/student.py
@@ -6,13 +6,13 @@ import json
 from tornado.web import RequestHandler
 
 
-class StudentsHandler(RequestHandler):
+class CallbackStudentsHandler(RequestHandler):
     def on_response(self, response):
         print("**************")
         if response.error:
             self.send_error(500)
         else:
-            data=json.loads(response.body)
+            data = json.loads(response.body)
             self.write(str(data))
         self.finish()
 
@@ -29,3 +29,34 @@ class StudentsHandler(RequestHandler):
 class HomeHandler(RequestHandler):
     def get(self, *args, **kwargs):
         self.write("home")
+
+
+class CoroutineStudentsHandler(RequestHandler):
+    @tornado.web.gen.coroutine
+    def get(self, *args, **kwargs):
+        url = "https://jsonplaceholder.typicode.com/comments"
+        client = AsyncHTTPClient()
+        res = yield client.fetch(url)
+        if res.error:
+            self.send_error(500)
+        else:
+            data = json.loads(res.body)
+            self.write(str(data))
+
+
+class RefineCoroutineStudentsHandler(RequestHandler):
+    @tornado.gen.coroutine
+    def get(self, *args, **kwargs):
+        res = yield self.getData()
+        self.write(str(res))
+
+    @tornado.gen.coroutine
+    def getData(self):
+        url = "https://jsonplaceholder.typicode.com/comments"
+        client = AsyncHTTPClient()
+        result = yield client.fetch(url)
+        if result.error:
+            ret={"ret":0}
+        else:
+            ret=json.loads(result.body)
+        raise tornado.gen.Return(ret)
