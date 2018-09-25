2f6ffc962a7d781ab7b45486bfbfd049d962ce73
zuohd <zuo.houde@gmail.com>
2018-09-26 04:45:24 +0800

    custom admin page of tables

--- django_project/project/myApp/admin.py
+++ django_project/project/myApp/admin.py
@@ -1,1 +1,18 @@
+from django.contrib import admin
+
 # Register your models here.
+from .models import Grades, Children
+
+
+class GradesAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'gname', 'gdate', 'ggirlnum', 'gboynum', 'isDeleted']
+    list_filter = ['gname']
+    search_fields = ['gname']
+    list_per_page = 5
+
+    # fields = ['ggirlnum','gboynum','gname','gdate','isDeleted']
+    # fieldsets = []
+
+
+admin.site.register(Grades, GradesAdmin)
+admin.site.register(Children)
