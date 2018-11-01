105bad97e37b0f0aa48e6f13a0a942494420815e
zuohd <zuo.houde@gmail.com>
2018-11-01 12:46:42 +0800

    add router

--- flask_project/app/app.py
+++ flask_project/app/app.py
@@ -5,8 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def Hello_world():
-    return 'Hello world!'
+    return '<h1>Hello world!</h1>'
+
+
+@app.route('/welcome/<name>/<uid>/')
+def welcome(name, uid):
+    return 'Hello {} {}'.format(uid, name)
+
+
+@app.route('/user/<int:uid>/')
+def user(uid):
+    return 'Hello {}'.format(uid)
+
+
+@app.route('/path/<path:p>')
+def path(p):
+    return p
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
